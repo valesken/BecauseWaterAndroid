55a726a0eb896dc800e89fbf7246f7bef7112c95
valesken <jeff.valesken@gmail.com>
2014-11-16 17:36:17 -0500

    Temporary fragment and response for Add button.

--- DropIn/gen/org/becausewater/dropin/R.java
+++ DropIn/gen/org/becausewater/dropin/R.java
@@ -1657,11 +1657,13 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f050030;
         public static final int action_mode_bar_stub=0x7f05002f;
         public static final int action_mode_close_button=0x7f050022;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050023;
+        public static final int add_new=0x7f050046;
         public static final int always=0x7f05000d;
         public static final int beginning=0x7f050012;
         public static final int bottom=0x7f050016;
+        public static final int bottom_bar=0x7f050045;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000f;
         public static final int container=0x7f050042;
@@ -1765,9 +1767,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_home=0x7f030017;
-        public static final int fragment_home=0x7f030018;
-        public static final int fragment_navigation_drawer=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int fragment_add=0x7f030018;
+        public static final int fragment_home=0x7f030019;
+        public static final int fragment_navigation_drawer=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int global=0x7f0c0000;
