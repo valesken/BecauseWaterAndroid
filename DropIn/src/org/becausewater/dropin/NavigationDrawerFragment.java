f01bcd25c15cbce9c531c8bea2b28c644527838f
valesken <jeff.valesken@gmail.com>
2014-12-29 18:53:29 -0500

    Minimal update - just finishing for the day - may have fixed a "bug" which was causing the nav menu to auto-select the first option every time I loaded the app

--- DropIn/src/org/becausewater/dropin/NavigationDrawerFragment.java
+++ DropIn/src/org/becausewater/dropin/NavigationDrawerFragment.java
@@ -72,11 +72,6 @@ public class NavigationDrawerFragment extends Fragment {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
         mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
 
-        if (savedInstanceState != null) {
-            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
-            mFromSavedInstanceState = true;
-            selectItem(mCurrentSelectedPosition); // Select the last selected item.
-        }
     }
 
     @Override
