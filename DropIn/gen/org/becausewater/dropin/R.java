2eb9b22b0379b673ebee9b684879f0d66f437117
valesken <jeff.valesken@gmail.com>
2015-01-03 12:20:54 -0500

    Updated launcher icon.

--- DropIn/gen/org/becausewater/dropin/R.java
+++ DropIn/gen/org/becausewater/dropin/R.java
@@ -2021,17 +2021,18 @@ containing a value of this type.
         public static final int common_signin_btn_text_pressed_dark=0x7f02004d;
         public static final int common_signin_btn_text_pressed_light=0x7f02004e;
         public static final int drawer_shadow=0x7f02004f;
-        public static final int ic_action_add=0x7f020050;
-        public static final int ic_drawer=0x7f020051;
-        public static final int ic_launcher=0x7f020052;
-        public static final int ic_plusone_medium_off_client=0x7f020053;
-        public static final int ic_plusone_small_off_client=0x7f020054;
-        public static final int ic_plusone_standard_off_client=0x7f020055;
-        public static final int ic_plusone_tall_off_client=0x7f020056;
-        public static final int nav_button_background=0x7f020057;
-        public static final int nav_button_text=0x7f020058;
-        public static final int powered_by_google_dark=0x7f020059;
-        public static final int powered_by_google_light=0x7f02005a;
+        public static final int drop_in_launcher=0x7f020050;
+        public static final int ic_action_add=0x7f020051;
+        public static final int ic_drawer=0x7f020052;
+        public static final int ic_launcher=0x7f020053;
+        public static final int ic_plusone_medium_off_client=0x7f020054;
+        public static final int ic_plusone_small_off_client=0x7f020055;
+        public static final int ic_plusone_standard_off_client=0x7f020056;
+        public static final int ic_plusone_tall_off_client=0x7f020057;
+        public static final int nav_button_background=0x7f020058;
+        public static final int nav_button_text=0x7f020059;
+        public static final int powered_by_google_dark=0x7f02005a;
+        public static final int powered_by_google_light=0x7f02005b;
     }
     public static final class id {
         public static final int about_because_water_button=0x7f080069;
