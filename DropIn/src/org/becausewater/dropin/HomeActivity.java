c160e53ed8f64406f02107934f9b50ae6cbedfd9
valesken <jeff.valesken@gmail.com>
2015-01-04 18:05:39 -0500

    Updated "Contact Us" page with icons

--- DropIn/src/org/becausewater/dropin/HomeActivity.java
+++ DropIn/src/org/becausewater/dropin/HomeActivity.java
@@ -37,6 +37,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
